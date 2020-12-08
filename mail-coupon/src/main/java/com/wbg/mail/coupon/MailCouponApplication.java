584590d68351f0f5444cc689f62f4b11acb3254d
wbg-global <wbg2963815global@qq.com>
2020-12-08 16:42:50 +0800

    Original Version

---
+++ mail-coupon/src/main/java/com/wbg/mail/coupon/MailCouponApplication.java
@@ -0,0 +1,13 @@
+package com.wbg.mail.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailCouponApplication.class, args);
+    }
+
+}
